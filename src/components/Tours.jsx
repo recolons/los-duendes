b652674ccf6d2f652fa204a84682dfb4704dfd8e
Ruben Colon <kikecolon@Rubens-MacBook-Air.local>
2025-12-13 16:36:16 -0400

    add elements and styling to tours component

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 
 const Tours = () => {
   return (
+    // add images to the tours section
+    // replace placeholder text with actual text
     <div className="bg-white text-black w-full py-16 px-4">
         <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-4">
-            <img className='w-[500px] h-[500px] mx-auto my-4 bg-red-300' src='' alt="/" />
-            <div>
-                <p>Municipio, PR</p>
-                <h1>Via Ferrata</h1>
+            <img className='w-[200px] h-[200px] mx-auto bg-red-300' src='' alt="/" />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] uppercase font-bold'>Municipio, PR</p>
+                <h1 className='text-3xl font-bold py-3'>Via Ferrata</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
-                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4'>Read More</button>
+                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
             </div>
-            <img src='' alt="/" />
-            <div>
-                <p>Municipio, PR</p>
-                <h1>Via Ferrata</h1>
+            <img className='w-[200px] h-[200px] mx-auto bg-red-300' src='' alt="/" />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] uppercase font-bold'>Municipio, PR</p>
+                <h1 className='text-3xl font-bold py-3'>Full Adventure</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
-                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4'>Read More</button>
+                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
             </div>
         </div>
     </div>
