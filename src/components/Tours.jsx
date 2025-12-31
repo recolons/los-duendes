0e246f3b284cdc4b5c82bb33eefb0a1816e0f384
Ruben Colon <kikecolon@Rubens-MacBook-Air.local>
2025-12-31 19:34:56 -0400

    add styling to tours component in home, add elements to about page.

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -4,24 +4,28 @@ const Tours = () => {
   return (
     // add images to the tours section
     // replace placeholder text with actual text
-    // add hover effect from cards component in video
-    <div className="bg-white text-gray-900 w-full py-16 px-4">
-        <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-4">
-            <img className='w-[90%] h-[200px] mx-auto bg-red-300' src='' alt="/" />
-            <div className='flex flex-col justify-center mb-3 mx-auto'>
-                <p className='text-green-600 uppercase font-bold'>Municipio, PR</p>
-                <h1 className='text-3xl font-bold py-3'>Via Ferrata</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
-                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
-            </div>
-            <img className='w-[90%] h-[200px] mx-auto bg-red-300' src='' alt="/" />
-            <div className='flex flex-col justify-center mb-3 mx-auto'>
-                <p className='text-[#00df9a] uppercase font-bold'>Municipio, PR</p>
-                <h1 className='text-3xl font-bold py-3'>Full Adventure</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
-                <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
-            </div>
+    <div className='text-gray-900'>
+      <div className="w-screen mx-auto h-40 bg-gray-400 flex items-center justify-center">
+        <h1 className='text-5xl text-center font-medieval'>Our Tours</h1>
+      </div>
+      <div className="bg-white w-full py-16 px-4">
+        <div className="max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-4 [&_button]:hover:bg-gray-800 [&_button]:transition-colors [&_button]:duration-300">
+          <img className='w-[90%] h-[200px] mx-auto bg-red-300' src='' alt="/" />
+          <div className='flex flex-col justify-center mb-3 mx-auto'>
+            <p className='text-green-600 uppercase font-bold'>Municipio, PR</p>
+            <h1 className='text-3xl font-bold py-3'>Via Ferrata</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
+            <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
+          </div>
+          <img className='w-[90%] h-[200px] mx-auto bg-red-300' src='' alt="/" />
+          <div className='flex flex-col justify-center mb-3 mx-auto'>
+            <p className='text-green-600 uppercase font-bold'>Municipio, PR</p>
+            <h1 className='text-3xl font-bold py-3'>Full Adventure</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, aliquid sit ducimus deleniti eaque culpa beatae iusto necessitatibus temporibus autem!</p>
+            <button className='border w-[100px] rounded-sm font-medium bg-black text-white p-2 mt-4 hover:bg-gray-800'>Read More</button>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -6,7 +6,19 @@ const About = () => {
     document.title = 'About | Travelling Duende'
   }, [])
   return (
-    <div>About</div>
+    <div className='text-gray-900'>
+        <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col items-center justify-center'>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 font-medieval'>About Us</h1>
+            <div>
+                {/* maybe change py-4 to py-2 or remove*/}
+                <p className='md:text-3xl sm:text-2xl text-xl font-bold py-4'>We are a team of adventure seekers who are passionate about connecting with nature through our tours.</p>
+            </div>
+            <p className='md:text-2xl text-xl text-gray-600 my-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, blanditiis laborum eligendi eveniet veritatis ex sed dolor accusantium quas sint esse, culpa natus? Laborum quae culpa vel dolorem molestias cum, ullam ad at officia laboriosam unde corrupti? Eum dolores possimus sed odio quos velit reiciendis minus! Neque alias sunt beatae.
+            </p>
+        </div>
+        
+    </div>
   )
 }
 
